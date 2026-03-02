62084c3025a6700551d603b9da7885add66ccc0a
KSuchita <kondetisuchita@gmail.com>
2026-03-02 19:46:43 +0530

    Fixed model path issue in backend

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,16 @@ from fastapi import FastAPI
 import yfinance as yf
 import joblib
 import pandas as pd
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
-
-model = joblib.load("../model.pkl")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (for development)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+model = joblib.load("../realtime_rf_model.pkl")
 
 STOCK_OPTIONS = {
     "Reliance": "RELIANCE.NS",
